b7635455986cae6c4bff791bf60ddfb306dc9906
sergeybarbolin <sergeybarbolin25@gmail.com>
2019-08-11 15:21:03 +0300

    Layout wrapping

--- src/LayoutWrapping/LayoutWrapping.js
+++ src/LayoutWrapping/LayoutWrapping.js
@@ -5,4 +5,6 @@ import React, { Component } from 'react';
   со стилем 'position: absolute'
 */
 
-export const wrapWithAbsolutePosition = () => {}
+export const wrapWithAbsolutePosition = (WrapedComponent) => () => (
+  <div style={{position: 'absolute'}}><WrapedComponent/></div>
+)
